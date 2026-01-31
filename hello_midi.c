b3762ccfba88231a63d2a6ef4ae0313f2537caca
Logan <logannbjork1@gmail.com>
2026-01-31 09:45:17 -0500

    is it working

--- hello_midi.c
+++ hello_midi.c
@@ -6,6 +6,10 @@
 #include "bsp/board_api.h"
 #include "tusb.h"
 
+// DEFINE ALL NOTE POS
+#define KICK 36
+#define SNARE 38
+#define CLOSED_HAT 60
 
 enum  {
   BLINK_NOT_MOUNTED = 250,
@@ -21,7 +25,12 @@ void midi_task(void);
 /*------------- MAIN -------------*/
 int main(void) {
   board_init();
+  stdio_init_all();
+  adc_init();
 
+  adc_gpio_init(26);
+  gpio_set_dir(0, false);
+  
   // init device stack on configured roothub port
   tusb_rhport_init_t dev_init = {
     .role = TUSB_ROLE_DEVICE,
@@ -36,6 +45,53 @@ int main(void) {
     led_blinking_task();
     midi_task();
   }
+}
+
+//--------------------------------------------------------------------+
+// MIDI Task
+//--------------------------------------------------------------------+
+
+void midi_task(void)
+{
+  // static uint32_t start_ms = 0;
+  uint8_t const cable_num = 0; // MIDI jack associated with USB endpoint
+  uint8_t const channel = 0; // 0 for channel 1
+
+  // The MIDI interface always creates input and output port/jack descriptors
+  // regardless of these being used or not. Therefore incoming traffic should be read
+  // (possibly just discarded) to avoid the sender blocking in IO
+  while (tud_midi_available()) {
+    uint8_t packet[4];
+    tud_midi_packet_read(packet);
+  }
+
+  // Select ADC input 0 (GPIO26)
+  adc_select_input(0);
+  uint16_t result = adc_read();
+  
+  // calculate velocity 
+  const float bit = ((float)((1 << 7) - 1))/((float)((1 << 12) - 1));
+  uint16_t velocity = result * bit * 1.5;
+
+  const float conversion_factor = 3.3f / (1 << 12);
+  double volt = result * conversion_factor;
+  
+  if (volt >= 0.2) {
+    if (velocity < 30) {
+        velocity = 30;
+    } else if (velocity > 115) {
+        velocity = 115;
+    }
+    // Send Note On for current position at full velocity (127) on channel 1.
+    uint8_t note_on[3] = { 0x90 | channel, SNARE, velocity };
+    tud_midi_stream_write(cable_num, note_on, 3);
+
+    // Send Note Off for previous note.
+    uint8_t note_off[3] = { 0x80 | channel, SNARE, 0};
+    tud_midi_stream_write(cable_num, note_off, 3);
+    sleep_ms(500);
+  }
+
 }
 
 //--------------------------------------------------------------------+
@@ -66,71 +122,6 @@ void tud_resume_cb(void) {
 }
 
 //--------------------------------------------------------------------+
-// MIDI Task
-//--------------------------------------------------------------------+
-
-// Variable that holds the current position in the sequence.
-uint32_t note_pos = 0;
-
-// Store example melody as an array of note values
-const uint8_t note_sequence[] = {
-  74,78,81,86,90,93,98,102,57,61,66,69,73,78,81,85,88,92,97,100,97,92,88,85,81,78,
-  74,69,66,62,57,62,66,69,74,78,81,86,90,93,97,102,97,93,90,85,81,78,73,68,64,61,
-  56,61,64,68,74,78,81,86,90,93,98,102
-};
-
-void midi_task(void)
-{
-  static uint32_t start_ms = 0;
-
-  uint8_t const cable_num = 0; // MIDI jack associated with USB endpoint
-  uint8_t const channel   = 0; // 0 for channel 1
-
-  // The MIDI interface always creates input and output port/jack descriptors
-  // regardless of these being used or not. Therefore incoming traffic should be read
-  // (possibly just discarded) to avoid the sender blocking in IO
-  while (tud_midi_available()) {
-    uint8_t packet[4];
-    tud_midi_packet_read(packet);
-  }
-
-  // REMOVE
-  // send note periodically
-  if (board_millis() - start_ms < 286) {
-    return; // not enough time
-  }
-  start_ms += 286;
-
-  // Previous positions in the note sequence.
-  int previous = (int) (note_pos - 1);
-
-  // If we currently are at position 0, set the
-  // previous position to the last note in the sequence.
-  if (previous < 0) {
-    previous = sizeof(note_sequence) - 1;
-  }
-  // TS
-
-  // Send Note On for current position at full velocity (127) on channel 1.
-  uint8_t note_on[3] = { 0x90 | channel, note_sequence[note_pos], 127 };  // TODO: ALTER note_seq to just regular note, and channel to smth else?
-  tud_midi_stream_write(cable_num, note_on, 3);
-
-  // Send Note Off for previous note.
-  uint8_t note_off[3] = { 0x80 | channel, note_sequence[previous], 0};
-  tud_midi_stream_write(cable_num, note_off, 3);
-
-  // REMOVE
-  // Increment position
-  note_pos++;
-  
-  // If we are at the end of the sequence, start over.
-  if (note_pos >= sizeof(note_sequence)) {
-    note_pos = 0;
-  }
-  // TS
-}
-
-//--------------------------------------------------------------------+
 // BLINKING TASK
 //--------------------------------------------------------------------+
 void led_blinking_task(void)
@@ -145,27 +136,3 @@ void led_blinking_task(void)
   board_led_write(led_state);
   led_state = 1 - led_state; // toggle
 }
-
-/*
-int main() {
-    stdio_init_all();
-    adc_init();
-    tusb_init();
-
-    adc_gpio_init(26); 
-    adc_gpio_init(27); 
-    adc_gpio_init(28); 
-    
-
-    while (1) {
-        const float conversion_factor = 3.3f / (1 << 12);
-
-        // might need to do this for each pin depending on how circuit structures multiplexor 
-        adc_select_input(0);
-        float val = adc_read();
-        
-        printf("Raw value: 0x%03x, voltage: %f V\n", val, val * conversion_factor);
-        sleep_ms(500);
-    }
-}
-*/
